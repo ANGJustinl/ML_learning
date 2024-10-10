75d4ac3f843f04fdf1152a06dd05f4e3fe324c28
angjustinl <96008766+ANGJustinl@users.noreply.github.com>
2024-10-10 16:32:31 +0800

    :rotating_light: Formatted with Black

--- MNIST_CNN/model_loader.py
+++ MNIST_CNN/model_loader.py
@@ -6,9 +6,9 @@ def load_checkpoint(model_path, model, optimizer) -> torch.nn.Module:
     Load the model and optimizer from the checkpoint file.
     """
     checkpoint = torch.load(model_path)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    epoch = checkpoint['epoch']
-    loss = checkpoint['loss']
+    model.load_state_dict(checkpoint["model_state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+    epoch = checkpoint["epoch"]
+    loss = checkpoint["loss"]
     model.eval()
     return model, optimizer, epoch, loss
